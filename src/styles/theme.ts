38d97dd59b6997da6098568bccebf861867b214a
Valdenício Ferreira <valdenicio22@gmail.com>
2022-05-03 01:39:05 -0300

    feat: change theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -20,14 +20,14 @@ export default {
       medium: '1.6rem',
       large: '1.8rem',
       xlarge: '2.0rem',
-      xxlarge: '2.8rem'
+      xxlarge: '4.5rem'
     }
   },
   colors: {
-    primary: '#F231A5',
-    secondary: '#3CD3C1',
-    mainBg: '#06092B',
-    white: '#FAFAFA',
+    primary: '#ec3f6e',
+    mainBg: '#141628',
+    secondaryBg: '#1b1e32',
+    white: '#FFFFFF',
     black: '#030517',
     lightGray: '#EAEAEA',
     gray: '#8F8F8F',
