5c53bcd6b785712ad81b61c91e11b795099d2a2d
Valdenício Ferreira <valdenicio22@gmail.com>
2022-05-03 01:49:28 -0300

    feat: fixed header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,17 +3,19 @@ import * as S from './styles'
 
 const Header = () => (
   <S.NavListContainer>
-    <S.NavList>
-      <S.NavItem>
-        <Link href="#work">Work</Link>
-      </S.NavItem>
-      <S.NavItem>
-        <Link href="#aboutMe">About</Link>
-      </S.NavItem>
-      <S.NavItem>
-        <Link href="#connect">Connect</Link>
-      </S.NavItem>
-    </S.NavList>
+    <S.NavListContent>
+      <S.NavList>
+        <S.NavItem>
+          <Link href="#work">Work</Link>
+        </S.NavItem>
+        <S.NavItem>
+          <Link href="#aboutMe">About</Link>
+        </S.NavItem>
+        <S.NavItem>
+          <Link href="#connect">Connect</Link>
+        </S.NavItem>
+      </S.NavList>
+    </S.NavListContent>
   </S.NavListContainer>
 )
 
