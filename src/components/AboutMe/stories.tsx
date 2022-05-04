ef5e48665fb2f156e4632e6c0b00937529303c40
Valdenício Ferreira <valdenicio22@gmail.com>
2022-05-03 22:08:20 -0300

    fix: remove unnecessary prop

--- src/components/AboutMe/stories.tsx
+++ src/components/AboutMe/stories.tsx
@@ -1,9 +1,9 @@
 import { Story, Meta } from '@storybook/react'
-import AboutMe, { AboutMeProps } from '.'
+import AboutMe from '.'
 
 export default {
   title: 'AboutMe',
   component: AboutMe
 } as Meta
 
-export const Default: Story<AboutMeProps> = (args) => <AboutMe {...args} />
+export const Default: Story = (args) => <AboutMe {...args} />
--- src/components/AboutMe/test.tsx
+++ src/components/AboutMe/test.tsx
@@ -4,6 +4,6 @@ import AboutMe from '.'
 
 describe('<AboutMe />', () => {
   it('should render the AboutMe', () => {
-    renderWithThemeProvider(<AboutMe id="aboutMe" />)
+    renderWithThemeProvider(<AboutMe />)
   })
 })
