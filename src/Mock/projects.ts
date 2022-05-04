0311f8ae2cfc93af016e89706bec707ec274a50f
Valdenício Ferreira <valdenicio22@gmail.com>
2022-05-04 01:38:52 -0300

    feat: add mock project

---
+++ src/Mock/projects.ts
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    name: 'joinme-ioasys-challenge',
+    description:
+      'Application developed for the last stage of the IV bootcamp of Ioasys. Meet new people and participate in events with activities that interest you. Enjoy your free time doing what you love!',
+    img: '/img/joinMe.png'
+  },
+  {
+    name: 'joinme-ioasys-challenge',
+    description:
+      'Application developed for the last stage of the IV bootcamp of Ioasys. Meet new people and participate in events with activities that interest you. Enjoy your free time doing what you love!',
+    img: '/img/joinMe.png'
+  },
+  {
+    name: 'joinme-ioasys-challenge',
+    description:
+      'Application developed for the last stage of the IV bootcamp of Ioasys. Meet new people and participate in events with activities that interest you. Enjoy your free time doing what you love!',
+    img: '/img/joinMe.png'
+  }
+]
