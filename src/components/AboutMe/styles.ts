13805b70955e400adc3854d5a06ebb40fc79fced
Valdenício Ferreira <valdenicio22@gmail.com>
2022-05-15 12:06:21 -0300

    feat: add media query

--- src/components/AboutMe/styles.ts
+++ src/components/AboutMe/styles.ts
@@ -1,22 +1,86 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 type Props = {
-  isAboutMeVisible: boolean
+  isAboutMeVisible?: boolean
 }
 
-export const Wrapper = styled.div`
-  max-width: ${({ theme }) => theme.containers.regularDesktop};
+export const AboutMeContainer = styled.div`
+  max-width: ${({ theme }) => theme.containers.desktop};
   width: 100%;
   margin: 0 auto;
 
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto;
+
+  ${media.greaterThan('medium')`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 0.5fr 2.5fr;
+    grid-template-areas:
+    'IntroContainer .'
+    'AboutMeInfoContainer PhotoContainer';
+    gap: 5rem 3rem;
+  `}
+
+  ${media.greaterThan('large')`
+    gap: 7rem 5rem;
+  `}
+`
+export const IntroContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-direction: column;
+
+  ${media.greaterThan('medium')`
+    grid-area: IntroContainer;
+  `}
+`
+
+export const Title = styled.h2`
+  font-size: ${({ theme }) => theme.font.sizes.xxlarge};
+  margin: 3rem 0;
+`
+export const PhotoContainer = styled.div`
+  width: 100%;
   display: flex;
   align-items: center;
-  gap: 2rem;
+  justify-content: center;
+  margin-bottom: 4rem;
+
+  ${media.greaterThan('medium')`
+    grid-area: PhotoContainer;
+
+    align-items: flex-start;
+  `}
+`
+
+export const Img = styled.img`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 25rem;
+  height: 25rem;
+  border-radius: 50%;
+  filter: grayscale(90%);
+
+  ${media.greaterThan('medium')`
+    width: 32rem;
+    height: 32rem;
+  `}
+
+  ${media.greaterThan('large')`
+    width: 40rem;
+    height: 40rem;
+  `}
 `
 
 const AboutMeModifiers = {
   isVisible: () => css`
-    opacity: 1;
+    opacity: 0;
   `
 }
 
@@ -24,84 +88,65 @@ export const AboutMeInfoContainer = styled.div<Props>`
   ${({ isAboutMeVisible }) => css`
     max-width: 68rem;
     width: 100%;
-    opacity: 0;
+    opacity: 1;
     transition: opacity 250ms ease-in;
 
-    ${isAboutMeVisible && AboutMeModifiers.isVisible()}
+    ${isAboutMeVisible && AboutMeModifiers.isVisible()};
+
+    ${media.greaterThan('medium')`
+      grid-area: AboutMeInfoContainer;
+    `}
   `}
 `
 
-export const SmallTitleContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 2rem;
-`
-
-export const SmallTitle = styled.p`
-  font-weight: ${({ theme }) => theme.font.weight.bold};
-  font-size: ${({ theme }) => theme.font.sizes.large};
-`
-
-export const Title = styled.h2`
-  font-size: ${({ theme }) => theme.font.sizes.xlarge};
-  margin-top: 3.2rem;
-  margin-bottom: 10rem;
-`
-
-//Who am I section description
 export const DetailsContainer = styled.div`
-  padding-bottom: 4rem;
+  padding-bottom: 3rem;
 `
+//Marker Who am I
 
-export const Description = styled.ul`
+export const DetailsList = styled.div`
   padding-top: 2rem;
+`
+export const Item = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
 
-  .person {
-    svg {
-      margin-right: -0.2rem;
+    & > svg {
+      max-width: 2.5rem;
+      width: 100%;
+      color: ${theme.colors.primary};
     }
-  }
+
+    ${media.greaterThan('medium')`
+      & > svg {
+        max-width: 3rem;
+        width: 100%;
+      }
+    `}
+
+    margin-bottom: 1.5rem;
+  `}
 `
 
-export const Item = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-
-  margin-bottom: 1rem;
-  font-size: ${({ theme }) => theme.font.sizes.large};
-
-  strong {
-    font-weight: ${({ theme }) => theme.font.weight.bold};
-  }
+//Marker Skill & Tools
+export const DescriptionX = styled.ul`
+  padding-top: 2rem;
+  list-style: none;
 `
 
 export const SkillsToolsItem = styled.p`
-  margin-bottom: 1rem;
-  font-size: ${({ theme }) => theme.font.sizes.large};
+  ${({ theme }) => css`
+    margin-bottom: 1rem;
 
-  strong {
-    font-weight: ${({ theme }) => theme.font.weight.bold};
-  }
-`
+    strong {
+      font-weight: ${theme.font.weight.bold};
+      font-size: ${theme.font.sizes.medium};
 
-export const AboutMePhotoContainer = styled.div`
-  width: 58rem;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
-
-export const Photo = styled.div`
-  width: fit-content;
-  height: fit-content;
-
-  img {
-    width: 32rem;
-    height: 32rem;
-
-    border-radius: 50%;
-    filter: grayscale(90%);
-  }
+      ${media.greaterThan('medium')`
+        font-size: ${theme.font.sizes.large};
+      `}
+    }
+  `}
 `
