99d91c59e2cdccf2ec4fd5417525e1bbfc5e4921
Valdenício Ferreira <valdenicio22@gmail.com>
2022-05-14 21:17:58 -0300

    feat: create mediaMatch component

---
+++ src/components/MediaMatch/test.tsx
@@ -0,0 +1,8 @@
+import MediaMatch from '.'
+import { renderWithThemeProvider } from '../../utils/renderWithThemeProvider'
+
+describe('<MediaMatch />', () => {
+  it('should render the MediaMatch', () => {
+    renderWithThemeProvider(<MediaMatch />)
+  })
+})
